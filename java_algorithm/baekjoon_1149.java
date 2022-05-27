a08c8b4ed286452c5584f70cd811b95dba037fda
svdvss <43364585+svdvss@users.noreply.github.com>
2022-05-27 15:33:16 +0900

    Add files via upload

---
+++ java_algorithm/baekjoon_1149.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] rgbArr = new int[N][3];
+
+        for(int i = 0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j<3; j++)
+                rgbArr[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 1; i<N; i++){
+            rgbArr[i][0] += Math.min(rgbArr[i-1][1], rgbArr[i-1][2]);
+            rgbArr[i][1] += Math.min(rgbArr[i-1][0], rgbArr[i-1][2]);
+            rgbArr[i][2] += Math.min(rgbArr[i-1][0], rgbArr[i-1][1]);
+        }
+
+        int min = Math.min(Math.min(rgbArr[N-1][0], rgbArr[N-1][1]), rgbArr[N-1][2]);
+
+        System.out.println(min);
+    }
+}
