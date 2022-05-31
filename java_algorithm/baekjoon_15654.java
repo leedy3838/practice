df39eea83652ce584cbc0d717a7860ef867dee3e
svdvss <43364585+svdvss@users.noreply.github.com>
2022-05-31 21:19:21 +0900

    Add files via upload

---
+++ java_algorithm/baekjoon_15654.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int[] intArr;
+    static int[] dfsArr;
+    static boolean[] visited;
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        intArr = new int[N];
+        dfsArr = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++)
+            intArr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(intArr);
+
+        dfs(0);
+    }
+
+    static void dfs(int cnt){
+        if(cnt == M){
+            for(int i = 0; i<M; i++)
+                System.out.print(dfsArr[i]+" ");
+            System.out.println();
+
+            return;
+        }
+
+        for(int i = 0; i<N; i++){
+            if(!visited[i]) {
+                dfsArr[cnt] = intArr[i];
+                visited[i] = true;
+                dfs(cnt+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
