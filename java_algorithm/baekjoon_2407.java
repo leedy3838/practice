1dabf6b4a11bb1c4f1f3d629da5ed8dc0852fe2a
svdvss <43364585+svdvss@users.noreply.github.com>
2022-06-02 12:17:12 +0900

    백준 2407번 : 조합

--- java_algorithm/baekjoon_2407.java
+++ java_algorithm/baekjoon_2407.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.math.BigInteger;
 
-public class Main{
+public class Main{ 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
