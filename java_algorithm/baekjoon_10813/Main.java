9ff4907c4060c81ae6d6acb48fac843fed154c48
LDY <leedy3838@naver.com>
2023-11-10 19:25:10 +0900

    백준 10813 : 공 바꾸기

---
+++ java_algorithm/baekjoon_10813/Main.java
@@ -0,0 +1,35 @@
+package baekjoon_10813;
+
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] array = new int[N+1];
+        for(int i = 1; i <= N; i++)
+            array[i] = i;
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            int tmp = array[A];
+            array[A] = array[B];
+            array[B] = tmp;
+        }
+
+        for(int i = 1; i <= N; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
