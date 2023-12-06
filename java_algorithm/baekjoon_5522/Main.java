89ff2bbfc362e1942d32fe219814c13c1f1cfab9
LDY <leedy3838@naver.com>
2023-12-06 10:20:17 +0900

    백준 5522 : 카드 게임

---
+++ java_algorithm/baekjoon_5522/Main.java
@@ -0,0 +1,18 @@
+package baekjoon_5522;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long sum = 0;
+
+        for(int i = 0; i < 5; i++) {
+            sum += Long.parseLong(br.readLine());
+        }
+
+        System.out.println(sum);
+    }
+}
