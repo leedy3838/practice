05bbf5d8d67b7cca49a69d71eb192588e0449f21
LDY <leedy3838@naver.com>
2023-12-08 09:44:49 +0900

    백준 30676 : 이 별은 무슨 색일까

---
+++ java_algorithm/baekjoon_30676/Main.java
@@ -0,0 +1,28 @@
+package baekjoon_30676;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (N <= 780&& N >= 620)
+            System.out.println("Red");
+        else if (N >= 590)
+            System.out.println("Orange");
+        else if (N >= 570)
+            System.out.println("Yellow");
+        else if (N >= 495)
+            System.out.println("Green");
+        else if (N >= 450)
+            System.out.println("Blue");
+        else if (N >= 425)
+            System.out.println("Indigo");
+        else
+            System.out.println("Violet");
+    }
+}
