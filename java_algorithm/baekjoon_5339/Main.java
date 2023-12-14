c78fc0a7cd9908a232aa6ea0f84f24ad11790638
LDY <leedy3838@naver.com>
2023-12-14 12:21:42 +0900

    백준 5339 : 콜센터

---
+++ java_algorithm/baekjoon_5339/Main.java
@@ -0,0 +1,22 @@
+package baekjoon_5339;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append("     /~\\\n");
+        sb.append("    ( oo|\n");
+        sb.append("    _\\=/_\n");
+        sb.append("   /  _  \\\n");
+        sb.append("  //|/.\\|\\\\\n");
+        sb.append(" ||  \\ /  ||\n");
+        sb.append("============\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+
+        System.out.print(sb);
+    }
+}
