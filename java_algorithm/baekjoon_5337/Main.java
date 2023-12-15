ebe0ea7d6268f3aca1bec021e9afb7716745d67e
LDY <leedy3838@naver.com>
2023-12-15 21:55:09 +0900

    백준 5337 : 웰컴

---
+++ java_algorithm/baekjoon_5337/Main.java
@@ -0,0 +1,17 @@
+package baekjoon_5337;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(".  .   .\n");
+        sb.append("|  | _ | _. _ ._ _  _\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.\n");
+
+        System.out.print(sb);
+    }
+}
