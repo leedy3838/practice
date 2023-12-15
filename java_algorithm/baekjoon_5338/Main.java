e96d96340cccd3cb18693742d0773af00f8cd022
LDY <leedy3838@naver.com>
2023-12-16 08:12:49 +0900

    백준 5338 : 마이크로소프트 로고

---
+++ java_algorithm/baekjoon_5338/Main.java
@@ -0,0 +1,17 @@
+package baekjoon_5338;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("       _.-;;-._\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-;;-._|\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-''-._|\n");
+
+        System.out.print(sb);
+    }
+}
