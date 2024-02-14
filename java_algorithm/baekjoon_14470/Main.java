0a8c3c053d3d1b8e44d231452bca8b8335a857e8
LDY <leedy3838@naver.com>
2024-02-14 19:52:53 +0900

    14470번 : 전자레인지

---
+++ java_algorithm/baekjoon_14470/Main.java
@@ -0,0 +1,28 @@
+package baekjoon_14470;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        if (A < 0) {
+            sum = (Math.abs(A) * C) + D + (B * E);
+        } else if (A == 0){
+            sum = D + (B * E);
+        } else {
+            sum = (B - A) * E;
+        }
+
+        System.out.println(sum);
+    }
+}
