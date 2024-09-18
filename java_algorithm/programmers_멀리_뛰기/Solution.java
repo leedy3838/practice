ac340fc0961d4c2c065f5c874c0fdda4ca9e3c08
leedy3838 <leedy3838@naver.com>
2024-09-18 12:00:16 +0900

    programmers: 멀리 뛰기

---
+++ java_algorithm/programmers_멀리_뛰기/Solution.java
@@ -0,0 +1,17 @@
+package programmers_멀리_뛰기;
+
+class Solution {
+    public long solution(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        
+        if (n > 1)
+            dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1_234_567;
+        }
+        
+        return dp[n];
+    }
+}
