b971a81923bcb50daeb9a6ffe6fc5b86124015f6
leedy3838 <leedy3838@naver.com>
2024-09-27 14:45:38 +0900

    programmers: 택배 배달과 수거하기 (refactor)

--- java_algorithm/programmers_150369/Solution.java
+++ java_algorithm/programmers_150369/Solution.java
@@ -4,67 +4,17 @@ class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
 
-        int idx = n - 1;
-        while (idx >= 0) {
-            if (deliveries[idx] == 0 && pickups[idx] == 0) {
-                idx--;
-            } else {
-                break;
+        int deliver = 0, pickup = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            deliver += deliveries[i];
+            pickup += pickups[i];
+
+            while (deliver > 0 || pickup > 0) {
+                deliver -= cap;
+                pickup -= cap;
+                answer += (i + 1) * 2;
             }
-        }
-
-        while (idx >= 0) {
-            int deliverIdx = idx;
-            int pickIdx = idx;
-            int deliverBox = 0;
-            int pickBox = 0;
-
-            while (deliverBox < cap) {
-                if (deliverIdx < 0) {
-                    break;
-                }
-                if (deliveries[deliverIdx] == 0) {
-                    deliverIdx--;
-                    continue;
-                }
-
-                if (deliverBox + deliveries[deliverIdx] <= cap) {
-                    deliverBox += deliveries[deliverIdx];
-                    deliveries[deliverIdx--] = 0;
-
-                    while (deliverIdx >= 0 && deliveries[deliverIdx] == 0) {
-                        deliverIdx--;
-                    }
-                } else {
-                    deliveries[deliverIdx] -= cap - deliverBox;
-                    deliverBox = cap;
-                }
-            }
-
-            while (pickBox < cap) {
-                if (pickIdx < 0) {
-                    break;
-                }
-                if (pickups[pickIdx] == 0) {
-                    pickIdx--;
-                    continue;
-                }
-
-                if (pickBox + pickups[pickIdx] <= cap) {
-                    pickBox += pickups[pickIdx];
-                    pickups[pickIdx--] = 0;
-
-                    while (pickIdx >= 0 && pickups[pickIdx] == 0) {
-                        pickIdx--;
-                    }
-                } else {
-                    pickups[pickIdx] -= cap - pickBox;
-                    pickBox = cap;
-                }
-            }
-
-            answer += (idx + 1) * 2;
-            idx = Math.max(deliverIdx, pickIdx);
         }
 
         return answer;
