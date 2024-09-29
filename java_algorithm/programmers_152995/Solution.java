70c2533afb0242e45d48c71e969f1032c9c93495
leedy3838 <leedy3838@naver.com>
2024-09-29 14:35:54 +0900

    programmers: 인사고과 (refactor)

--- java_algorithm/programmers_152995/Solution.java
+++ java_algorithm/programmers_152995/Solution.java
@@ -3,35 +3,22 @@ package programmers_152995;
 import java.util.*;
 
 class Solution {
-    int[][] scores;
     public int solution(int[][] scores) {
-        this.scores = scores;
-        
-        int answer = 1;
-        
-        int sum = scores[0][0] + scores[0][1];
-        for (int[] score : scores) {
-            if (sum < score[0] + score[1]) {
-                if (check(score)) {
-                    answer++;
-                }
+        int[] s = scores[0];
+        Arrays.sort(scores, (s1, s2) -> s1[0] == s2[0] ? s1[1] - s2[1] : s2[0] - s1[0]);
+
+        int max = 0, ans = 1;
+        for(int[] score : scores) {
+            if(score[1] < max) {
+                if(score.equals(s))
+                    return -1;
+            } else {
+                max = Math.max(max, score[1]);
+                if(s[0] + s[1] < score[0] + score[1])
+                    ans++;
             }
         }
-        
-        for (int[] score : scores) {
-            if (scores[0][0] < score[0] && scores[0][1] < score[1])
-                return -1;
-        }
-        
-        return answer;
-    }
-    
-    private boolean check(int[] checkArr) {
-        for (int[] score : scores) {
-            if (score[0] > checkArr[0] && score[1] > checkArr[1])
-                return false;
-        }
-        
-        return true;
+
+        return ans;
     }
 }
